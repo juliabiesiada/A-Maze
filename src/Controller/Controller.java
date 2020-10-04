6cc18344e39a578bb5a3ede00776d8a7e8e33dc2
juliabiesiada <jbiesiada@outlook.com>
2020-10-04 17:49:13 +0200

    Commit 3: Panes added

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,11 +1,34 @@
 package Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class Controller {
+	
+	@FXML
+	private GridPane board_grid;
+	@FXML
+	private List<Pane> panes;
 	
 	public void onClicked(MouseEvent mouseEvent) {
 		//code for ending turn
 		System.out.println("test");
+	}
+	
+	public void initialize() {
+		panes = new ArrayList<Pane>();
+		for (int i = 0; i<7; i++) {
+			for (int j = 0; j<7; j++) {
+				Pane pane = new Pane();
+				pane.setId("p"+i+j);
+				panes.add(pane);
+				board_grid.add(pane, i, j);
+			}
+		}
 	}
 }
